0ba3eab281f0bcd14e62f2deaeb2ceb69a5d8d8c
nic-str <3697620+nic-str@users.noreply.github.com>
2022-01-01 18:58:06 +0100

    Prepare for logging

---
+++ modules/setup_logger.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+""""""
+"""
+Created on Tue Dec 28 19:03:40 2021
+
+@author: Nicolas Striebig
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
